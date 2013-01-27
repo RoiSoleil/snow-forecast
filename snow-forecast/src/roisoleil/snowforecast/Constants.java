03bde630a782b94290a748bacc166ad47a3854ff
helios.gilles@gmail.com <helios.gilles@gmail.com@b0cac987-560e-9a4a-132a-9a15d59584ec>
2013-01-27 17:08:59 +0000

    HG : Intial commit

---
+++ snow-forecast/src/roisoleil/snowforecast/Constants.java
@@ -0,0 +1,25 @@
+package roisoleil.snowforecast;
+
+public class Constants {
+
+	public final static String AREA = "area";
+
+	public final static String ID = "id";
+
+	public final static String AREA_NAME = "areaname";
+
+	public final static String STATE = "state";
+
+	public final static String NEW_SNOW = "newsnow";
+
+	public final static String BASE = "base";
+
+	public final static String METRIC = "metric";
+
+	public final static String CLOSED = "closed";
+
+	public final static String N = "N";
+
+	public final static String Y = "Y";
+	
+}
